72596c68d8143532b2735630287141f35ede7833
Simon Halimi <halimisimon@gmail.com>
2021-03-15 23:20:58 +0100

    feat(): add tests

---
+++ tests/test.test.ts
@@ -0,0 +1,11 @@
+import { expect } from "chai"
+
+describe("test", () => {
+    it("should properly launch tests", () => {
+        expect(false).to.be.false;
+    })
+
+    it("should properly launch tests", () => {
+        expect(true).to.be.true;
+    })
+})
